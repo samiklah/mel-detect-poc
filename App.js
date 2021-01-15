d08b358271d7494c6f083bc753e37eb0897bef3f
Sami Klah <samikla7@gmail.com>
2021-01-15 15:53:41 +0200

    camira added

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ Created: 21/08/2020
 Last modified: 24/08/2020
 */
 
+import {StatusBar} from 'expo-status-bar';
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import * as tf from "@tensorflow/tfjs";
@@ -12,6 +13,8 @@ import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 import * as jpeg from "jpeg-js";
 import Output from "./Output";
+
+import {Camera} from 'expo-camera';
 
 async function getPermissionAsync() {
   if (Constants.platform.ios) {
@@ -147,7 +150,32 @@ export default function App() {
     console.log(error);
   }
 
+  const [startCamera,setStartCamera] = React.useState(false);
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState(null);
+
+  const __startCamera = async () => {
+    const {status} = await Camera.requestPermissionsAsync()
+    if (status === 'granted') {
+      // start the camera
+      setStartCamera(true)
+    } else {
+      Alert.alert('Access denied')
+    }
+  }
+  const __takePicture = async () => {
+    if (!camera) return
+    const photo = await camera.takePictureAsync()
+    console.log(photo)
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+   
+  }
+
+
+
   return (
+    
     <View style={styles.container}>
       <View style={styles.innercontainer}>
         <Text style={styles.status}>
@@ -165,6 +193,78 @@ export default function App() {
           />
         </TouchableOpacity>
       </View>
+
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <TouchableOpacity
+        onPress={__startCamera}
+          style={{
+            width: 130,
+            borderRadius: 4,
+            backgroundColor: '#14274e',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 40
+          }}
+        >
+          <Text
+            style={{
+              color: '#fff',
+              fontWeight: 'bold',
+              textAlign: 'center'
+            }}
+          >
+            Take picture
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Camera
+      style={{flex: 1,width:"100%"}}
+      ref={(r) => {
+      camera = r
+      }}
+      ></Camera>
+
+      <StatusBar style="auto" />
+
+      <View
+        style={{
+        position: 'absolute',
+        bottom: 0,
+        flexDirection: 'row',
+        flex: 1,
+        width: '100%',
+        padding: 20,
+        justifyContent: 'space-between'
+        }}
+      >
+        <View
+        style={{
+        alignSelf: 'center',
+        flex: 1,
+        alignItems: 'center'
+        }}
+        >
+            <TouchableOpacity
+            onPress={__takePicture}
+            style={{
+            width: 70,
+            height: 70,
+            bottom: 0,
+            borderRadius: 50,
+            backgroundColor: '#fff'
+            }}
+            />
+    </View>
+    </View>
     </View>
   );
 }
